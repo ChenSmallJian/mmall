1e2441fc5c5b96c18792e6ecedb7def0e1587ad8
chenjian <whut_chj@163.com>
2017-11-23 09:26:55 +0800

    finished the shipping paart,and has passed the test

--- src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mmall.service.impl;
 
+import com.github.pagehelper.PageInfo;
 import com.google.common.collect.Maps;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.ShippingMapper;
@@ -8,6 +9,7 @@ import com.mmall.service.IShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,7 +23,7 @@ public class ShippingServiceImpl implements IShippingService {
 
     public ServerResponse add(Integer userId, Shipping shipping) {
         shipping.setUserId(userId);
-        int rowCount = shippingMapper.insertSelective(shipping);
+        int rowCount = shippingMapper.insert(shipping);
         if (rowCount > 0) {
             Map result = Maps.newHashMap();
             result.put("shippingId", shipping.getId());
@@ -38,7 +40,7 @@ public class ShippingServiceImpl implements IShippingService {
         return ServerResponse.createByErrorMessage("删除地址失败");
     }
 
-    public ServerResponse<String> update(Integer userId,Shipping shipping){
+    public ServerResponse<String> update(Integer userId, Shipping shipping) {
         // 为了防止越权，从登录用户中获取用户id，并赋予shipping中
         shipping.setUserId(userId);
         int resultCount = shippingMapper.updateByUserIdAndShippingId(shipping);
@@ -47,4 +49,20 @@ public class ShippingServiceImpl implements IShippingService {
         }
         return ServerResponse.createByErrorMessage("更新地址失败");
     }
+
+    public ServerResponse<Shipping> select(Integer userId, Integer shippingId) {
+        Shipping shipping = shippingMapper.selectByUserIdAndShippingId(userId, shippingId);
+        if (shipping == null) {
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("查询地址成功", shipping);
+    }
+
+    public ServerResponse<PageInfo> list(Integer userId, int pageNum, int PageSize) {
+        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
+
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccessMessage(pageInfo);
+    }
+
 }
