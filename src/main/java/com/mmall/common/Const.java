9e674be5f6b298477b168e55829bac99eae018c6
chenjian <whut_chj@163.com>
2017-11-24 21:50:04 +0800

    finished the zhifubao part,and has passed the test

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -10,11 +10,11 @@ public class Const {
     public static final String EMAIL = "email";
     public static final String USERNAME = "username";
 
-    public interface ProductListOrderBy{
-        Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_asc","price_desc");
+    public interface ProductListOrderBy {
+        Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_asc", "price_desc");
     }
 
-    public interface Cart{
+    public interface Cart {
         int CHECKED = 1; // 即购物车选中状态
         int UNCHECKED = 0; // 购物车未选中
 
@@ -28,7 +28,7 @@ public class Const {
     }
 
     public enum ProductStatusEnum {
-        ON_SALE("在线",1);
+        ON_SALE("在线", 1);
         private String value;
         private int code;
 
@@ -36,6 +36,58 @@ public class Const {
             this.value = value;
             this.code = code;
         }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public enum OrderStatusEnum {
+        CANCELED("已取消", 0),
+        NO_PAY("未支付", 10),
+        PAID("已付款", 20),
+        SHIPPED("已发货", 40),
+        ORDER_SUCCESS("订单完成", 50),
+        ORDER_CLOSE("订单关闭", 60);
+
+        OrderStatusEnum(String value, int code) {
+            this.value = value;
+            this.code = code;
+        }
+
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+    public interface AlipayCallback{
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+    }
+
+    public enum PayPlatformEnum{
+        ALIPAY("支付宝",1);
+        ;
+        PayPlatformEnum(String value, int code) {
+            this.value = value;
+            this.code = code;
+        }
+
+        private String value;
+        private int code;
 
         public String getValue() {
             return value;
--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,7 +1,17 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
 /**
  * created by chenjian
  */
 public interface IOrderService {
+
+    ServerResponse pay(Long orderNO, Integer userId, String path);
+
+    ServerResponse aliCallBack(Map<String,String> params);
+
+    ServerResponse<Boolean> queryOrderPayStatus(Integer userId, Long orderNo);
 }
